40918bcf5b14374b03b8c02a8a0789d859278d7d
Paulina Dyrek <paoladlugosz@gmail.com>
2023-10-24 13:05:01 +0200

    create the other part of services

--- app/components/method/page.js
+++ app/components/method/page.js
@@ -3,16 +3,69 @@ import styles from 'app/sass/offer.scss';
 export default function Offer(){
   return(
     <div className='work'>
-    <h1>Usługi jakie świadczymy</h1>
+      <div className='title-container'>
+        <h1>Usługi jakie świadczymy</h1>
+      </div>
       <div className='main'>
         <div className='divider'>
-          <h4>Rehabilitacja</h4>
+          <div className='img-box'>
+            <img src='/article1.jpg' />
+          </div>
+          <div className='description-box'>
+            <h2>Rehabilitacja</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.</p>
+          </div>
+        </div>
+        <div className='circle'>
+          <img src='/offer_1.png' />
+        </div> 
+      </div>
+      <div className='main'>
+        <div className='divider'>
+          <h2>Terapia manualna</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.</p>
+        </div>
+        <div className='circle'>
+          <img src='/offer_2.png' />
+        </div> 
+      </div>
+      <div className='main'>
+        <div className='divider'>
+          <h2>Terapia neurofizjologiczna/ortopedyczna</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.</p>
+        </div>
+        <div className='circle'>
+          <img src='/offer_3.png' />
+        </div> 
+      </div>
+      <div className='main'>
+        <div className='divider'>
+          <h2>Terapia wisceralna</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.</p>
+        </div>
+        <div className='circle'>
+          <img src='/offer_4.png' />
+        </div> 
+      </div>
+      <div className='main'>
+        <div className='divider'>
+          <h2>Suche igłowanie</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.</p>
+        </div>
+        <div className='circle'>
+          <img src='/offer_5.png' />
+        </div> 
+      </div>
+      <div className='main'>
+        <div className='divider'>
+          <h2>Trenig medyczny</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi quaerat quis natus quod. Quisquam, corrupti et, temporibus deleniti natus sequi non repellendus facilis architecto similique commodi ratione? Ipsa, quod.</p>
         </div>
         <div className='circle'>
           <img src='/offer_1.png' />
         </div> 
       </div>
+
       {/* <div className='main'>
         <div className='divider'>diveder</div>
         <div className='circle'>circle2</div> 
