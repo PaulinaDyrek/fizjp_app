3abf1df1e56d17f771530792f8839cc844cd2b28
Paulina Dyrek <paoladlugosz@gmail.com>
2023-11-07 11:01:22 +0100

    team wrapper

--- app/pages/Team.js
+++ app/pages/Team.js
@@ -3,7 +3,15 @@ import styles from 'app/sass/team.scss';
 
 export default function Team(){
   return(
-    <div></div>
+    <div className='team_wrapper'>
+      <h1 className='team__title'>Poznaj nasz zespół</h1>
+      <div className='team__profiles'>
+        <img src='/team/fizjo_1.jpg' className='profile-img' />
+        <h3 className="user-name">Rafał</h3>
+        <h5>Fizjoterapeuta</h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatum fugit tempore obcaecati adipisci?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, exercitationem animi enim fuga alias.</p>
+      </div>
+    </div>
 
   )
 }
