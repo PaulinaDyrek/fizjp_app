26238855a7a28d7c35338bad45c2b36d4b26cb12
Paulina Dyrek <paoladlugosz@gmail.com>
2023-12-18 11:37:42 +0100

    clear page js

--- app/page.js
+++ app/page.js
@@ -4,13 +4,12 @@ import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import layout from './layout.js';
 import Menu from './pages/Menu.js';
+import Header from './pages/Header.js';
 import Home from './pages/Home.js';
 import About from './pages/About.js';
-import Slider from './pages/Slider.js';
 import ContactForm from './pages/contact/page.js';
 import Review from './pages/Review.js';
 import Methods from './pages/Methods.js';
-import Header from './pages/Header.js';
 import Team from './pages/Team.js';
 import Footer from './pages/Footer.js';
 import Maps from './pages/Maps.js';
@@ -21,8 +20,6 @@ export default function Web() {
     <layout id='__next'>
       <Menu></Menu>
       <Header></Header>
-      {/* <Slider></Slider> */}
-      {/* <Logo></Logo> */}
       <Home></Home>
       {/* <About></About> */}
       <Methods></Methods>
