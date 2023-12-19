433828e6162f75aa217aa34d9664f725f3b0167f
Paulina Dyrek <paoladlugosz@gmail.com>
2023-12-19 13:33:49 +0100

    responsive 'team'

--- app/pages/Team.js
+++ app/pages/Team.js
@@ -3,32 +3,29 @@ import styles from 'app/sass/team.scss';
 
 export default function Team(){
   return(
-    <div className='team_wrapper'>
-      <h1 className='team__title'><span>Poznaj</span> nasz zespół</h1>
-      <div className="profiles__section">
+    <div className='team'>
+      <h1 className='team__title'><span className='team__title-little'>Poznaj</span> nasz zespół</h1>
+      <div className="team__section">
         <div className='team__profiles'>
-          <img src='/team/1-fizjo.jpg' className='profile-img' />
-          <h3 className="user-name">Rafał</h3>
-          <h5>Fizjoterapeuta</h5>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatum fugit tempore obcaecati adipisci?Lorem ipsum. </p>
+          <img src='/team/1-fizjo.jpg' className='team__img' />
+          <h3 className='team__name'>Rafał</h3>
+          <h5 className='team__profession'>Fizjoterapeuta</h5>
+          <p className='team__about'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatum fugit tempore obcaecati adipisci?Lorem ipsum. </p>
         </div>
         <div className='team__profiles'>
-          <img src='/team/2-fizjo.jpg' className='profile-img' />
-          <h3 className="user-name">Marta</h3>
-          <h5>Terapeuta ruchowy</h5>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatum fugit tempore obcaecati adipisci?Lorem ipsum. </p>
+          <img src='/team/2-fizjo.jpg' className='team__img' />
+          <h3 className='team__name'>Marta</h3>
+          <h5 className='team__profession'>Terapeuta ruchowy</h5>
+          <p className='team__about'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatum fugit tempore obcaecati adipisci?Lorem ipsum. </p>
         </div>
         <div className='team__profiles'>
-          <img src='/team/3-fizjo.jpg' className='profile-img' />
-          <h3 className="user-name">Alicja</h3>
-          <h5>Fizjoterapeuta</h5>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatum fugit tempore obcaecati adipisci?Lorem ipsum.</p>
+          <img src='/team/3-fizjo.jpg' className='team__img' />
+          <h3 className='team__name'>Alicja</h3>
+          <h5 className='team__profession'>Fizjoterapeuta</h5>
+          <p className='team__about'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatum fugit tempore obcaecati adipisci?Lorem ipsum.</p>
         </div>
       </div>
-      
-      
     </div>
-
   )
 }
 
