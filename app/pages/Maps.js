f2c492ba460bec07578c69630f5f930862fa25dc
Paulina Dyrek <paoladlugosz@gmail.com>
2024-01-03 13:24:32 +0100

    add google maps at contact form

--- app/pages/Maps.js
+++ app/pages/Maps.js
@@ -3,31 +3,35 @@ import React from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import styles from 'app/sass/maps.scss'
 
+export default function Maps () {
+  return(
+    <div ><iframe className='map'  src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=%C5%81ukowica%20440+(Fizjostacja)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
+  )
+}
 
+// const Map = () => {
+//   const mapStyles = {
+//     height: '500px',
+//     width: '100%',
+//   };
 
-const Map = () => {
-  const mapStyles = {
-    height: '500px',
-    width: '100%',
-  };
+//   const defaultCenter = {
+//     lat: 40.712776,
+//     lng: -74.005974,
+//   };
 
-  const defaultCenter = {
-    lat: 40.712776,
-    lng: -74.005974,
-  };
+//   return (
+//     <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY}>
+//       <GoogleMap className='map'
+//         mapContainerStyle={mapStyles}
+//         zoom={10}
+//         center={defaultCenter}
+//       ></GoogleMap>
+//     </LoadScript>
+//   );
+// };
 
-  return (
-    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY}>
-      <GoogleMap className='map'
-        mapContainerStyle={mapStyles}
-        zoom={10}
-        center={defaultCenter}
-      ></GoogleMap>
-    </LoadScript>
-  );
-};
-
-export default Map;
+// export default Map;
 
 
 
